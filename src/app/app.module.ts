64350b8bdf16de5529f4643099ce335ddfdbfd0e
igor041 <33355401+igor041@users.noreply.github.com>
2020-11-08 14:59:00 -0700

    Initial checkin.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './components/recipe-edit/recipe-edit.component';
+import { RecipeManagerComponent } from './components/recipe-manager/recipe-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecipeListComponent,
+    RecipeEditComponent,
+    RecipeManagerComponent
   ],
   imports: [
     BrowserModule,
